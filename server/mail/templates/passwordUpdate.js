f1b1ba1c3f7ccf6a50082ab805600a449c27ba43
RiddhiMisra <144816153+RiddhiMisra@users.noreply.github.com>
2024-07-30 01:21:57 +0530

    Update passwordUpdate.js

--- server/mail/templates/passwordUpdate.js
+++ server/mail/templates/passwordUpdate.js
@@ -55,7 +55,7 @@ exports.passwordUpdated = (email, name) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
+            <a href="https://edu-roan-chi.vercel.app"><img class="logo"
                     src="https://i.ibb.co/c2vbYk4/eduquest.png" alt="Eduquest Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
